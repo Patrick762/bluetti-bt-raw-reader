ef676d539ffed8cb9fdf3db71d74818303e96f9c
Patrick762 <lutzpatti86@gmail.com>
2025-11-28 21:39:41 +0100

    added simple tool to display registers

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # bluetti-bt-raw-reader
-Tool to find new fields in raw data captured with bluetti-bt-lib
+Tool to find new fields in raw data captured with [bluetti-bt-lib](https://github.com/Patrick762/bluetti-bt-lib)
+
+To run the tool webserver, follow these steps:
+
+```bash
+npm i
+npm start
+```
+
+After that you can open the tool at http://localhost:8080/
---
+++ src/scripts.js
@@ -0,0 +1,113 @@
+// region Elements
+
+const fileInput = document.getElementById("file");
+
+const dataOutputDiv = document.getElementById("dataOutput");
+const macOutput = document.getElementById("mac");
+const iotOutput = document.getElementById("iot");
+const encOutput = document.getElementById("enc");
+const tableOutput = document.getElementById("tbody");
+
+// endregion
+
+// region Types
+
+/**
+ * @typedef {Object} DeviceData
+ * @property {string} mac
+ * @property {number} iotVersion
+ * @property {boolean} encryption
+ * @property {Object} registers
+ */
+
+// endregion
+
+/**
+ * @param {string} hex
+ */
+function splitHex(hex) {
+    const result = [];
+    for (let i = 0; i < hex.length; i += 4) {
+        result.push(hex.substring(i, i + 4));
+    }
+    return result;
+}
+
+/**
+ * @param {DeviceData} obj 
+ */
+function displayData (obj)
+{
+    macOutput.textContent = obj.mac;
+    iotOutput.textContent = obj.iotVersion;
+    encOutput.textContent = obj.encryption ? "YES" : "NO";
+
+    const registerKeys = Object.keys(obj.registers);
+
+    let filteredRegisters = [];
+    registerKeys.forEach(k => {
+        if (obj.registers[k] !== "" && obj.registers[k] !== "00")
+        {
+            filteredRegisters.push(...splitHex(obj.registers[k]));
+        }
+        else
+        {
+            for (var i = 0; i < 50; i++)
+            {
+                filteredRegisters.push("0000");
+            }
+        }
+    });
+    
+    console.log(filteredRegisters);
+
+    // Clear table
+    tableOutput.innerHTML = "";
+
+    filteredRegisters.forEach((reg, i) => {
+        const row = document.createElement("tr");
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+        td1.textContent = i;
+        td2.textContent = reg;
+
+        let str = "";
+
+        for (var j = 0; j < reg.length; j+=2)
+        {
+            str += String.fromCharCode(parseInt(reg.substring(j, j+2), 16));
+        }
+
+        td3.textContent = str;
+
+        row.appendChild(td1);
+        row.appendChild(td2);
+        row.appendChild(td3);
+        tableOutput.appendChild(row);
+    });
+
+    // Show data fully loaded
+    dataOutputDiv.style.display = "block";
+}
+
+document.getElementById("form").addEventListener("submit", e => {
+    e.preventDefault();
+
+    if(file.files.length)
+    {
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            const dataURL = reader.result;
+            const base64 = dataURL.slice(dataURL.indexOf(',')+1);
+            const data = atob(base64);
+            const obj = JSON.parse(data);
+            displayData(obj);
+        };
+
+        reader.readAsDataURL(file.files[0]);
+    }
+});
+
+dataOutputDiv.style.display = "none";
